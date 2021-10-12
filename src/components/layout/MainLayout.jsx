40394d797e1673a49878f3bdf5013ff0c6ce5a9b
Defrian Yarfi <defrian.yarfi>
2021-10-12 14:22:05 +0700

    chore: updates

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Col, Row, Container } from 'reactstrap';
+
+// import HeadDefault from './components/HeadDefault';
+import NavbarTop from '../navbar/NavbarTop';
+import NavbarLeft from '../navbar/NavbarLeft';
+import Footer from '../Footer';
+
+function MainLayout(props) {
+  const { children } = props;
+  return (
+    <>
+      <NavbarTop />
+      <Container fluid className="wrapper">
+        <Row>
+          <Col className="wrapper-left">
+            <NavbarLeft />
+          </Col>
+          <Col className="wrapper-content">{children}</Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
